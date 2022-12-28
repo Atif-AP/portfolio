0da67c33702d9f5cd4fbae29acf1497b4a0ce3e1
Atif-AP <s107214@ap.be>
2022-12-28 05:00:35 +0100

    Added portfolio styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,14 @@
-import { Outlet, createBrowserRouter, RouterProvider, NavLink } from "react-router-dom";
-import { useState } from 'react';
-import styles from "./App.module.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Calculator from "./components/portfolioComponents/Calculator";
 import ColorBox from "./components/portfolioComponents/Colorbox";
 import Filtering from "./components/portfolioComponents/Filtering";
 import Interval from "./components/portfolioComponents/Interval";
 import ShoppingList from "./components/portfolioComponents/Shoppinglist";
 import TicTacToe from "./components/portfolioComponents/TicTacToe";
-
-const Root = () => {
-  return (
-    <div className={styles.container}>
-        <div className={styles.header}>
-            <NavLink className={styles.link} to="/" >01 Home</NavLink>
-            <NavLink className={styles.link} to="portfolio">02 Portfolio</NavLink>
-            <NavLink className={styles.link} to="contact">03 Contact</NavLink>
-        </div>
-        <div>
-            <Outlet/>
-        </div>
-    </div>
-);
-}
-
-const Home = () => {
-  return(
-    <>
-      <div className={styles.intro}>
-        <p>Hi, my name is<br></br>
-        <span className={styles.name}>Atif Khalil</span><br></br>
-        <span className={styles.title}>Full Stack Developer</span><br></br>
-        I'm a Full Stack Developer mostly into Front End using React</p> 
-      </div>
-      <div>
-
-      </div>
-    </>
-  )
-}
-
-const Portfolio = () => {
-  return(
-    <div>
-      <div>
-        <NavLink to="/components/portfolioComponents/calculator">Calculator App</NavLink>
-      </div>
-      <div>
-        <NavLink to="/components/portfolioComponents/colorbox">ColorBox App</NavLink>
-      </div>
-      <div>
-        <NavLink to="/components/portfolioComponents/filtering">Filtering App</NavLink>
-      </div>
-      <div>
-        <NavLink to="/components/portfolioComponents/interval">Interval App</NavLink>
-      </div>
-      <div>
-        <NavLink to="/components/portfolioComponents/shoppinglist">Shopping List App</NavLink>
-      </div>
-      <div>
-        <NavLink to="/components/portfolioComponents/tictactoe">TicTacToe App</NavLink>
-      </div>
-      <div>
-        Placeholder
-      </div>
-      <div>
-        Placeholder
-      </div>
-    </div>
-  )
-};
-
-const Contact = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const handleSubmit = () => {
-    setTimeout(() => {
-      setSubmitted(true);
-    }, 100)
-  };
-
-  if (submitted) {
-    return(
-      <>
-        <div>Thank you!</div>
-        <div>I will be in touch!</div>
-      </>
-    )
-  }
-
-  return(
-    <>
-    <form action='' onSubmit={handleSubmit} method='post'>
-      <label>Name</label>
-      <input type='text' placeholder='Your Name' name='name' required></input>
-      <label>E-mail</label>
-      <input type='email' placeholder='Your Email' name='email' required></input>
-      <label>Message</label>
-      <textarea placeholder='Coding a Message...' name='message' required></textarea>
-      <button type='submit'>Send Message</button>
-
-    </form>
-    </>
-  )
-}
+import Root from "./components/Root"
+import Portfolio from "./components/Portfolio"
+import Home from "./components/Home"
+import Contact from "./components/Contact"
 
 const App = () => {
   const router = createBrowserRouter([
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,32 +1,80 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom"
 import styles from "./Portfolio.module.css"
 
 const Portfolio = () => {
     return(
-      <div>
-        <div>
-          <NavLink to="/components/portfolioComponents/calculator">Calculator App</NavLink>
+      <div className={styles.container}>
+        <div className={styles.childContainer}>
+            <h1>Calculator App</h1>
+            <p>App gemaakt met React state</p>
+            <Link to="/components/portfolioComponents/calculator">
+                <button>
+                    Go to App
+                </button>
+            </Link>
         </div>
-        <div>
-          <NavLink to="/components/portfolioComponents/colorbox">ColorBox App</NavLink>
+        <div className={styles.childContainer}>
+            <h1>Colorbox App</h1>
+            <p>App gemaakt met React state</p>
+            <Link to="/components/portfolioComponents/colorbox">
+                <button>
+                    Go to App
+                </button>
+            </Link>
         </div>
-        <div>
-          <NavLink to="/components/portfolioComponents/filtering">Filtering App</NavLink>
+        <div className={styles.childContainer}>
+            <h1>Filtering App</h1>
+            <p>App gemaakt met React state</p>
+            <Link to="/components/portfolioComponents/filtering">
+                <button>
+                    Go to App
+                </button>
+            </Link>
         </div>
-        <div>
-          <NavLink to="/components/portfolioComponents/interval">Interval App</NavLink>
+        <div className={styles.childContainer}>
+            <h1>Interval App</h1>
+            <p>App gemaakt met React state</p>
+            <Link to="/components/portfolioComponents/interval">
+                <button>
+                    Go to App
+                </button>
+            </Link>
         </div>
-        <div>
-          <NavLink to="/components/portfolioComponents/shoppinglist">Shopping List App</NavLink>
+        <div className={styles.childContainer}>
+            <h1>Shopping List App</h1>
+            <p>App gemaakt met React state</p>
+            <Link to="/components/portfolioComponents/shoppinglist">
+                <button>
+                    Go to App
+                </button>
+            </Link>
         </div>
-        <div>
-          <NavLink to="/components/portfolioComponents/tictactoe">TicTacToe App</NavLink>
+        <div className={styles.childContainer}>
+            <h1>Tic Tac Toe App</h1>
+            <p>App gemaakt met React state</p>
+            <Link to="/components/portfolioComponents/tictactoe">
+                <button>
+                    Go to App
+                </button>
+            </Link>
         </div>
-        <div>
-          Placeholder
+        <div className={styles.childContainer}>
+            <h1>PlaceHolder App</h1>
+            <p>App gemaakt met React state</p>
+            <Link to="/components/portfolioComponents/">
+                <button>
+                    Go to App
+                </button>
+            </Link>
         </div>
-        <div>
-          Placeholder
+        <div className={styles.childContainer}>
+            <h1>Placeholder App</h1>
+            <p>App gemaakt met React state</p>
+            <Link to="/components/portfolioComponents/">
+                <button>
+                    Go to App
+                </button>
+            </Link>
         </div>
       </div>
     )
