00b3e015430e7be6aa0da636ac9e6ef503d37851
Atif-AP <s107214@ap.be>
2022-12-29 07:19:38 +0100

    Added dark mode toggle

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,16 +1,18 @@
 import { Outlet, NavLink } from "react-router-dom"
 import styles from "./Root.module.css"
+import DarkMode from "./DarkMode";
 
 const Root = () => {
     return (
       <div className={styles.container}>
           <div className={styles.header}>
-              <NavLink className={styles.link} to="/" >01 Home</NavLink>
-              <NavLink className={styles.link} to="portfolio">02 Portfolio</NavLink>
-              <NavLink className={styles.link1} to="contact">03 Contact</NavLink>
+            <DarkMode/>
+            <NavLink className={styles.link} to="/" >01 Home</NavLink>
+            <NavLink className={styles.link} to="portfolio">02 Portfolio</NavLink>
+            <NavLink className={styles.link1} to="contact">03 Contact</NavLink>
           </div>
           <div>
-              <Outlet/>
+            <Outlet/>
           </div>
       </div>
   );
