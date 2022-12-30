5dbcb2cf67f24064a7b3c4dac71088980bda125a
Atif-AP <s107214@ap.be>
2022-12-30 08:21:49 +0100

    Working app on Vercel and CSS change

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -6,7 +6,7 @@ const Portfolio = () => {
       <div className={styles.container}>
         <div className={styles.childContainer}>
             <h1>Calculator App</h1>
-            <p>A caclulator app that uses React Hooks and Typescript</p>
+            <p className={styles.ptag}>A caclulator app that uses React Hooks and Typescript</p>
             <Link to="/components/portfolioComponents/calculator">
                 <button className={styles.pButton}>
                     Go to App
@@ -15,7 +15,7 @@ const Portfolio = () => {
         </div>
         <div className={styles.childContainer}>
             <h1>Colorbox App</h1>
-            <p>A Color picker type app that uses React hooks and Typescript</p>
+            <p className={styles.ptag}>A Color picker type app that uses React hooks and Typescript</p>
             <Link to="/components/portfolioComponents/colorbox">
                 <button className={styles.pButton}>
                     Go to App
@@ -24,7 +24,7 @@ const Portfolio = () => {
         </div>
         <div className={styles.childContainer}>
             <h1>Filtering App</h1>
-            <p>A filtering app that filters data using React/Typescript</p>
+            <p className={styles.ptag}>A filtering app that filters data using React/Typescript</p>
             <Link to="/components/portfolioComponents/filtering">
                 <button className={styles.pButton}>
                     Go to App
@@ -33,7 +33,7 @@ const Portfolio = () => {
         </div>
         <div className={styles.childContainer}>
             <h1>Interval App</h1>
-            <p>A interval app that updates it's data every so often using React/Typescript</p>
+            <p className={styles.ptag}>A interval app that updates it's data every so often using React/Typescript</p>
             <Link to="/components/portfolioComponents/interval">
                 <button className={styles.pButton}>
                     Go to App
@@ -42,7 +42,7 @@ const Portfolio = () => {
         </div>
         <div className={styles.childContainer}>
             <h1>Shopping List App</h1>
-            <p>A shopping list app that saves addable data to local storage using React/Typescript</p>
+            <p className={styles.ptag}>A shopping list app that saves addable data to local storage using React/Typescript</p>
             <Link to="/components/portfolioComponents/shoppinglist">
                 <button className={styles.pButton}>
                     Go to App
@@ -51,7 +51,7 @@ const Portfolio = () => {
         </div>
         <div className={styles.childContainer}>
             <h1>Tic Tac Toe App</h1>
-            <p>A Tic Tac Toe game made using React/Typescript</p>
+            <p className={styles.ptag}>A Tic Tac Toe game made using React/Typescript</p>
             <Link to="/components/portfolioComponents/tictactoe">
                 <button className={styles.pButton}>
                     Go to App
@@ -60,7 +60,7 @@ const Portfolio = () => {
         </div>
         <div className={styles.childContainer}>
             <h1>Slots App</h1>
-            <p>A Slots game that uses React/Typescript </p>
+            <p className={styles.ptag}>A Slots game that uses React/Typescript </p>
             <Link to="/components/portfolioComponents/slots">
                 <button className={styles.pButton}>
                     Go to App
@@ -69,7 +69,7 @@ const Portfolio = () => {
         </div>
         <div className={styles.childContainer}>
             <h1>Counter List App</h1>
-            <p>An App where u can add numbers and the app will create a sum using React/Typescript</p>
+            <p className={styles.ptag}>An App where u can add numbers and the app will create a sum using React/Typescript</p>
             <Link to="/components/portfolioComponents/counterlist">
                 <button className={styles.pButton}>
                     Go to App
@@ -78,7 +78,7 @@ const Portfolio = () => {
         </div>
         <div className={styles.childContainer}>
             <h1>Activity App</h1>
-            <p>App gemaakt met behulp van een API in React/Typescript</p>
+            <p className={styles.ptag}>App gemaakt met behulp van een API in React/Typescript</p>
             <Link to="/components/apiComponents/activity">
                 <button className={styles.pButton}>
                     Go to App
@@ -87,7 +87,7 @@ const Portfolio = () => {
         </div>
         <div className={styles.childContainer}>
             <h1>Relax Dog App</h1>
-            <p>App gemaakt met behulp van een API in React/Typescript</p>
+            <p className={styles.ptag}>App gemaakt met behulp van een API in React/Typescript</p>
             <Link to="/components/apiComponents/dog">
                 <button className={styles.pButton}>
                     Go to App
